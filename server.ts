6db5f4d7ef7ff85799e9b4dc63df097643b23add
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-29 22:02:07 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -12,18 +12,10 @@ app.use(
 app.use(helmet.hidePoweredBy());
 app.use(helmet.noSniff());
 app.use(helmet.xssFilter());
-app.use(
-	helmet({ crossOriginEmbedderPolicy: false })
-);
+app.use(helmet({ crossOriginEmbedderPolicy: false }));
 app.use(express.json({ limit: '50mb' }));
-app.get('/', (req, res) =>
-	res.send('Salut ca va ?')
-);
-app.use('*', (req, res) =>
-	res.send('Retour arrière frérot')
-);
+app.get('/', (req, res) => res.status(200).send('Salut ca va ?'));
+app.use('*', (req, res) => res.status(404).send('Retour arrière frérot'));
 app.listen(process.env.SERVER_PORT, () => {
-	console.log(
-		`Le serveur est OK sur le port ${process.env.SERVER_PORT}`
-	);
+	console.log(`Le serveur est OK sur le port ${process.env.SERVER_PORT}`);
 });
