ab6b59621bd63b3293dac96ffaf0ac032cc423e9
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-29 09:59:06 +0200

    Création de l'api squelette

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors'
+import helmet from 'helmet';
+const app = express();
+app.use(cors())
+app.use(helmet())
+app.use(helmet.dnsPrefetchControl({
+    allow: true
+}))
+app.use(helmet.hidePoweredBy())
+app.use(helmet.noSniff())
+app.use(helmet.xssFilter())
+app.use(helmet({ crossOriginEmbedderPolicy: false }))
+app.use(express.json({ limit: "50mb" }))
+app.get('/', (req, res) => res.send("Salut ca va ?"))
+app.use("*", (req, res) => res.send("Retour arrière frérot"))
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Le serveur est OK sur le port ${process.env.SERVER_PORT}`);
+});
