0ef38047004d5ac37c6c8099009ea7bd84c0c534
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-04-30 19:54:39 +0200

    Add IController & IMiddleware

---
+++ utils/interface.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+export type Middleware = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => NextFunction | Response;
+export type Controller = (req: Request, res: Response) => Response;
