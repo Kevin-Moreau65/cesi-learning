0deba0da279280f894a9237daf7c6fe257266474
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-05-08 11:26:29 +0200

    remake auth

--- server.ts
+++ server.ts
@@ -6,9 +6,6 @@ import * as bcrypt from 'bcrypt';
 import { user } from './models/user';
 import { Roles } from './utils/interface';
 import routerAuth from './routes/auth';
-import routerUser from './routes/user';
-import { checkTokenMiddleware } from './utils/jsonwebtoken';
-import routerSearch from './routes/search';
 const app = express();
 app.use(cors());
 app.use(helmet());
@@ -24,8 +21,6 @@ app.use(helmet({ crossOriginEmbedderPolicy: false }));
 app.use(express.json({ limit: '50mb' }));
 app.get('/', (req, res) => res.status(200).send('Salut ca va ?'));
 app.use('/login', routerAuth);
-app.use('/user', checkTokenMiddleware, routerUser);
-app.use('/search', checkTokenMiddleware, routerSearch);
 app.use('*', (req, res) => res.status(404).send('Retour arrière frérot'));
 mongoose
 	.connect(process.env.MONGO_URI)
