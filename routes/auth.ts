dc840fc2b1e1492f5dbe746a65bec7d5ee15e32b
Kevin-Moreau65 <79045771+Kevin-Moreau65@users.noreply.github.com>
2022-05-08 11:27:30 +0200

    remake user

--- routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import * as authCtrl from '../controllers/auth';
-const routerAuth = express.Router();
-
-routerAuth.post('/', authCtrl.login);
-
-export default routerAuth;
